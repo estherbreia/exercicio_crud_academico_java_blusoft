d1ce8e695b976288f94197c9c3a6e2cc631564bd
Guest Timbó <guest.tm@alunos.sc.senac.br>
2026-05-04 21:53:28 -0300

    alterações

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,13 +1,13 @@
 
-
-
-var done = 0;
-var usuario = document.getElementsByName('email').value.toLowerCase(); [^1^]
-var senha = document.getElementsByName('password').value.toLowerCase(); [^1^]
-if (usuario == 'usuario@senac.com' && senha == '1234') {
-window.location = 'portal.html';
-done = 1;
-}
-if (done == 0) {
-alert('Dados incorretos, tente novamente');
+function login () {
+    var done = 0;
+    var usuario = document.getElementsByName('email').value.toLowerCase(); [^1^]
+    var senha = document.getElementsByName('password').value.toLowerCase(); [^1^]
+    if (usuario == 'usuario@senac.com' && senha == '1234') {
+    window.location = 'portal.html';
+    done = 1;
+    }
+    if (done == 0) {
+    alert('Dados incorretos, tente novamente');
+    }
 }
