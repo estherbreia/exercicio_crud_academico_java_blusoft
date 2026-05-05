8abe8ea2c4f1ce1160f90fd26d266ca71d012a39
Esther Breia <estherbreia@gmail.com>
2026-05-05 10:22:12 -0300

    add function

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,13 +1,24 @@
 
-function login () {
-    var done = 0;
-    var usuario = document.getElementsByName('email').value.toLowerCase(); [^1^]
-    var senha = document.getElementsByName('password').value.toLowerCase(); [^1^]
+function login() {
+    var usuario = document.getElementsByName('email')[0].value.toLowerCase();
+    var senha = document.getElementsByName('password')[0].value.toLowerCase();
+
     if (usuario == 'usuario@senac.com' && senha == '1234') {
-    window.location = 'portal.html';
-    done = 1;
-    }
-    if (done == 0) {
-    alert('Dados incorretos, tente novamente');
+        window.location.href = 'portal.html';
+        return false;
+    } else {
+        alert('Dados incorretos, tente novamente');
+        return false;
     }
 }
+
+function mostrar(secao) {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('perfil').style.display = 'none';
+    document.getElementById('notas').style.display = 'none';
+    document.getElementById('contato').style.display = 'none';
+
+    document.getElementById(secao).style.display = 'block';
+}
+
+console.log("JS conectado!")
